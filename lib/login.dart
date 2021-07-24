ef9acec1425a947a81f429466170c97c0d45b89a
Tanmay Dewan <48048930+tanmaydewan@users.noreply.github.com>
2021-07-24 18:52:55 +0530

    updates

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:just_in_time/resources/location.dart';
 import 'package:just_in_time/screens/options.dart';
 import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 
