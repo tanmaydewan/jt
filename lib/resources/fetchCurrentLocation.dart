254ccc4eee7c847f02b1601e888ef6d708aeefb2
Tanmay Dewan <48048930+tanmaydewan@users.noreply.github.com>
2021-07-26 16:46:59 +0530

    camera add

--- lib/resources/fetchCurrentLocation.dart
+++ lib/resources/fetchCurrentLocation.dart
@@ -1,12 +1,20 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:just_in_time/resources/location.dart';
+import 'package:just_in_time/screens/registration_Screen.dart';
 import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 
-Future<void> saveReg(String dealerName, String address, String pincode, String taxNumber) async {
+Future<void> saveReg(String dealerName, String address, String pincode, String taxNumber, dynamic imagePath) async {
     await Future.delayed(Duration(seconds: 1), () {});
     Location lt = Location();
+    
+     
     await lt.getCurrentLocation();
     
     final location = ParseGeoPoint(latitude: lt.latitude , longitude: lt.longitude); 
-    final regObj = ParseObject('Dealer')..set('name', dealerName)..set('address', address)..set('location',location)..set('pincode', pincode)..set('TaxNumber', taxNumber);
+    final imagep =(Uri.file(imagePath));
+    
+    final regObj = ParseObject('Dealer')..set('name', dealerName)..set('address', address)..set('location',location)..set('pincode', pincode)..set('TaxNumber', taxNumber)..set("image",imagep );
     await regObj.save();
   }
--- lib/screens/registration_Screen.dart
+++ lib/screens/registration_Screen.dart
@@ -1,10 +1,16 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:just_in_time/resources/fetchCurrentLocation.dart';
+import 'package:just_in_time/resources/camera_Capture.dart';
 import 'package:just_in_time/screens/ReuseTile.dart';
 
 class Registration extends StatefulWidget {
   @override
   _RegistrationState createState() => _RegistrationState();
+
+  
 }
 
 class _RegistrationState extends State<Registration> {
@@ -13,11 +19,23 @@ class _RegistrationState extends State<Registration> {
   final controllerLocation = TextEditingController();
   final controllerPincode = TextEditingController();
   final controllerTaxNumber = TextEditingController();
+  late final  imagePath ;
+  late final k;
 
-
-
+  XFile? _image;
+  Future getImage() async {
+    final image = await ImagePicker().pickImage(source: ImageSource.camera);
+    // imagePath =image;
+     print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa${image!.path}");
+     
+    setState(() {
+      _image =image;
+      
+    });
+    imagePath=image.path;
+  } 
   void doUserRegistration() async {
-     await saveReg(controllerDealerName.text,controllerAddress.text,controllerPincode.text,controllerTaxNumber.text);
+     await saveReg(controllerDealerName.text,controllerAddress.text,controllerPincode.text,controllerTaxNumber.text,imagePath);
      
     setState(() {
       controllerDealerName.clear();
@@ -27,6 +45,7 @@ class _RegistrationState extends State<Registration> {
     });
   }
 
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,6 +63,54 @@ class _RegistrationState extends State<Registration> {
                 RegTextContainer(tController:controllerAddress, tLabel: 'Enter Address',tIcon: Icons.home_max_rounded,),
                 RegTextContainer(tController:controllerPincode, tIcon: Icons.pin_drop, tLabel: 'Enter Pincocde'),               
                 RegTextContainer(tController:controllerTaxNumber, tIcon: Icons.money_off_csred_rounded, tLabel:'Enter Tax Number'),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      height: 50,
+                      width: 50,
+                      child: Icon(Icons.camera_alt),
+                    ),
+          Center(
+          child: GestureDetector(
+            onTap: () {
+              getImage();
+            },
+            child: CircleAvatar(
+              radius: 55,
+              backgroundColor: Color(0xffFDCF09),
+              child: _image != null
+                  ? ClipRRect(
+                      borderRadius: BorderRadius.circular(50),
+                      child: Image.file(
+                        File(_image!.path),
+                        width: 100,
+                        height: 100,
+                        fit: BoxFit.fitHeight,
+                      ),
+                    )
+                  : Container(
+                      decoration: BoxDecoration(
+                          color: Colors.grey[200],
+                          borderRadius: BorderRadius.circular(50)),
+                      width: 100,
+                      height: 100,
+                      child: Icon(
+                        Icons.camera_alt,
+                        color: Colors.grey[800],
+                      ),
+                    ),
+            ),
+          ),
+        ),
+                       Container(
+                        height: 50,
+                        width: 50,
+                        child: Icon(Icons.camera_alt),
+                      ),
+                    
+                  ],
+                ),
                  Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
@@ -74,4 +141,5 @@ class _RegistrationState extends State<Registration> {
   }
 }
 
+  
 
